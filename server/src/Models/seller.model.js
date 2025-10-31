675e73ae09e2b3713351d52f18dfdea38db557d2
aabhushanCD <dammardhakal426@gmail.com>
2025-10-31 19:36:21 +0545

    wishlist and seller

---
+++ server/src/Models/seller.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const sellerSchema = new mongoose.Schema({
+  sellerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  shopName: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  contactEmail: {
+    type: String,
+    required: true,
+  },
+  document: {
+    pan: {
+      type: String,
+    },
+  },
+  phone: [
+    {
+      type: Number,
+    },
+  ],
+  rating: {
+    type: Number,
+    max: 5,
+  },
+  address: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Address",
+  },
+});
+
+const Seller = mongoose.model("Seller", sellerSchema);
+
+export default Seller;
