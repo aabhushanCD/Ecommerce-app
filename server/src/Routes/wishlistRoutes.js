f7c409c877a7da951e4381ea15d535a7e9cb0d25
aabhushanCD <dammardhakal426@gmail.com>
2025-11-13 13:47:09 +0545

    wishlist route added

---
+++ server/src/Routes/wishlistRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  clearWishlist,
+  toggleWishlist,
+  viewWishlist,
+} from "../Controllers/wishlistController.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+
+const router = express.Router();
+
+router.post("toggleWishlist", verifyToken, toggleWishlist);
+
+router.get("viewWishlist", verifyToken, viewWishlist);
+
+router.delete("clearWishlist", verifyToken, clearWishlist);
+
+export default router;
