f1d2393f05d33280332988493ec56158a0f518dd
aabhushanCD <dammardhakal426@gmail.com>
2025-11-13 12:03:02 +0545

    product route added

---
+++ server/src/Routes/productRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { addProduct, deleteProduct, getProducts, updateProductImage } from "../Controllers/productController.js";
+
+const router = express.Router();
+
+router.post("add/product", verifyToken, addProduct);
+router.put("update/product", verifyToken, addProduct);
+router.put("update/product/image",verifyToken,updateProductImage);
+
+router.get("view/product",verifyToken,getProducts);
+
+router.delete("remove/product",verifyToken,deleteProduct);
